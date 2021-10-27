39a43f93d811c71cc05f07c7aed9a238bb8e5fe6
Christian Webb <36595425+chriswebbb@users.noreply.github.com>
2021-10-27 20:41:55 +0100

    Update digitalPot.c

--- SPI/digitalPot.c
+++ SPI/digitalPot.c
@@ -1,7 +1,3 @@
-/*  Author: Steve Gunn
- * Licence: This work is licensed under the Creative Commons Attribution License.
- *          View this license at http://creativecommons.org/about/licenses/
- */
 #include <stdio.h>
 #include <stdlib.h>
 
